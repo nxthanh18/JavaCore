da2e1dcbb56d30e13d543f793f78fb90675a0b0b
Thanh <nxthanh18@gmail.com>
2019-02-21 22:53:34 +0700

    bai tap mang 14-16

---
+++ src/Arrays/Bai14.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author genharunari
+ * Viết chương trình nhập vào mảng một chiều số nguyên n phần tử (n > 0). Sắp xếp
+lẻ tăng dần nhưng giá trị khác giữ nguyên vị trí
+ */
+public class Bai14 {
+    public static Scanner sc = new Scanner(System.in);
+    public static void inputArray(int []array){
+        for(int i = 0; i < array.length;i++){
+            System.out.print("(" + i + ") = ");
+            array[i] = sc.nextInt();
+        }
+    }
+    public static void outputArray(int []array){
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + "; ");
+        }
+    }
+    public static void hoanVi(int []array){
+        int temp = array[0];
+        for(int i = 0; i < array.length; i++){
+            for(int j = i + 1; j < array.length; j++){
+                if(array[i]%2 == 1 && array[j]%2 == 1){
+                    if(array[i] > array[j]){
+                        temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+    public static void output(int []array){
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int n;
+        int []array;
+        
+        do{
+            System.out.print("n = ");
+            n = sc.nextInt();
+            if(n <= 0){
+                System.out.println("Input again n > 0");
+            }
+        }while(n <= 0);
+        
+        array = new int[n];
+        inputArray(array);
+        System.out.println("Mang vua nhap: ");
+        outputArray(array);
+        hoanVi(array);
+        System.out.println("Sap xep so le tang dan: ");
+        output(array);
+    }
+}
